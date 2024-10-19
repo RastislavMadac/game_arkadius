c8f6a581bfef7a133d7da78db360406fbe754185
Rastislav Madac <rastislavmadac@yahoo.co.uk>
2024-10-19 04:21:30 +0200

    fourth_step

--- main.py
+++ main.py
@@ -29,10 +29,31 @@ abilities = {
 }
 }
 
+
+
+def print_abilities_points():
+    for k,v in abilities.items():
+        print(k+" - "+ str(v["points"]), end=", ")
+    print("\n")
+
+def print_abilities_options():
+    for i, ability in enumerate(abilities.keys()):
+        ability_option= str(i+1)+' - '+ability
+        if ability=="Život":
+            ability_option+=""+"- jeden bod pridáva 5 života"
+        print(ability_option)
+
+def hero_choice_abilitie():
+    value= int(input(f"Máš {points} na zlepšenie. Ktorú schopnosť chceš vylepšiť?:"))
+    return value
+
+
+
 continue_game=True
 while continue_game:
     print(INTRO_TEXT)
-    intro_choice=input("Aká je tvoja voľba?")
+    #intro_choice=input("Aká je tvoja voľba?")
+    intro_choice="1"
     if intro_choice not in["0","1"]:
         print("Netrafil si ani jednu s možnú voľbu. Musím sa spítať znovu")
         continue
@@ -45,10 +66,12 @@ while continue_game:
     name_picked = False
     hero_name = ""
     while not name_picked:
-        name = input("Ako sa bude volať tvoj hrdina?: ")
+        name= "rasto"
+        #name = input("Ako sa bude volať tvoj hrdina?: ")
         print("Si si istý, ze sa tvoj hrdina bude volať " + name + "?")
         print("0 - Nie, chcem zmeniť meno\n1 - Áno")
-        name_choice = input("Aká je tvoja voľba?: ")
+        name_choice="1"
+        #name_choice = input("Aká je tvoja voľba?: ")
         if name_choice not in ["0", "1"]:
             print("Netrafil si ani jednu možnú voľbu. Musím sa ťa spýtať ešte raz.")
             continue
@@ -56,3 +79,42 @@ while continue_game:
             hero_name = name
             break
     print("Ahoj", hero_name)
+
+    abilities_information= f"{hero_name}, Tvoje schopnosti sú momentálne na tom takto:"
+    print(abilities_information)
+    print_abilities_points()
+
+    points = 7
+
+    abilities_options = f" Máš {points} bodov, ktoré si rozdel naprieč schopnostiam podľa svojich preferencii."
+    print(abilities_options)
+
+    while continue_game:
+        # f"Máš {points} na zlepšenie. Ktorú schopnosť chceš vylepšiť?
+        chosen_ability_index = hero_choice_abilitie() - 1
+        if 1 < points <= 7:
+
+            abilities_index = (list(abilities.keys())[chosen_ability_index])
+            points -= 1
+            actualiy_condition="Teraz tvoje body vyzerarajú takto"
+
+            if abilities_index== "Život":
+                abilities[abilities_index]["points"] += 5
+                print(f"Vybral si si možnosť - {abilities_index} pridávam ti 5 bodov.")
+                print( actualiy_condition)
+                print_abilities_points()
+            else:
+                 abilities[abilities_index]["points"] += 1
+                 print(f"Vybral si si možnosť - {abilities_index} pridávam ti bod.")
+                 print(actualiy_condition)
+                 print_abilities_points()
+
+        elif points==1:
+            print(f"Výborne {hero_name}. Dokončil si pridávanie schopnosťí. Pre rekapituláciu, teraz vyzerajú tvoje schopnosti takto.")
+            print_abilities_points()
+            break
+        else:
+            print("Netrafil si ani jednu možnú voľbu. Musím sa ťa spýtať ešte raz.")
+
+
+
