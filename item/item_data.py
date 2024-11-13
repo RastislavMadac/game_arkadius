0a511b14823d9cfe22f532c324663b93f3ad7eba
Rastislav Madac <rastislavmadac@yahoo.co.uk>
2024-11-13 11:23:24 +0100

    fifteenth-step

---
+++ item/item_data.py
@@ -0,0 +1,157 @@
+items = {
+    1: {
+        1: {
+            "name": "Nápoj zdravia (LVL 1)",
+            "description": "Pridáva 15 zdravia. Ak pri súboji zomrieš, tak ti nápoj vyprchá. Môžeš mať viacero nápojov naraz.",
+            "ability": {
+                "Život": 15
+            },
+            "tag": "drink",
+            "replaceable": False,
+            "destroy_when_lost": True
+        },
+        2: {
+            "name": "Látkové brnenie (LVL 1)",
+            "description": "Pridáva 2 obrany. Zostáva ti do konca hry, jedine že by si ho nahradil iným brnením.",
+            "ability": {
+                "Obrana": 2
+            },
+            "tag": "armor",
+            "replaceable": True,
+            "destroy_when_lost": False
+        },
+        3: {
+            "name": "Topánky (LVL 1)",
+            "description": "Pridáva 1 Obratnosť. Zostáva ti do konca hry, jedine že by si ich nahradil inými topánkami.",
+            "ability": {
+                "Obratnosť": 1
+            },
+            "tag": "boots",
+            "replaceable": True,
+            "destroy_when_lost": False
+        },
+    },
+    2: {
+        1: {
+            "name": "Nápoj šťastia (LVL 2)",
+            "description": "Pridáva 5 šťastia. Ak pri súboji zomrieš, tak ti nápoj vyprchá. Môžeš mať viacero nápojov naraz.",
+            "ability": {
+                "Šťastie": 5
+            },
+            "tag": "drink",
+            "replaceable": False,
+            "destroy_when_lost": True
+        },
+        2: {
+            "name": "Krátky meč (LVL 2)",
+            "description": "Pridáva 3 útočnej sily. Zostáva ti do konca hry, jedine že by si ho nahradil iným mečom.",
+            "ability": {
+                "Útočná sila": 3
+            },
+            "tag": "weapon",
+            "replaceable": True,
+            "destroy_when_lost": False
+        },
+        3: {
+            "name": "Tréning gladiátora (LVL 2)",
+            "description": "Pridáva 2 body skillu. Schopnosti ti nevyprchajú.",
+            "ability": {
+                "Skill": 2
+            },
+            "tag": "training",
+            "replaceable": False,
+            "destroy_when_lost": False
+        },
+    },
+    3: {
+        1: {
+            "name": "Gumidžús (LVL 3)",
+            "description": "Pridáva 2 body útočnej sile, 2 body obrane a 2 body skillu. Ak pri súboji zomrieš, tak ti nápoj vyprchá. Môžeš mať viacero nápojov naraz.",
+            "ability": {
+                "Útočná sila": 2,
+                "Obrana": 2,
+                "Skill": 2
+            },
+            "tag": "drink",
+            "replaceable": False,
+            "destroy_when_lost": True
+        },
+        2: {
+            "name": "Dlhý meč (LVL 3)",
+            "description": "Pridáva 5 útočnej sily. Zostáva ti do konca hry, jedine že by si ho nahradil iným mečom.",
+            "ability": {
+                "Útočná sila": 5
+            },
+            "tag": "weapon",
+            "replaceable": True,
+            "destroy_when_lost": False
+        },
+        3: {
+            "name": "Kožené topánky (LVL 3)",
+            "description": "Pridáva 2 obratnosti a 2 body obrany. Zostávajú ti do konca hry, jedine že by si ich nahradil inými topánkami.",
+            "ability": {
+                "Obratnosť": 2,
+                "Obrana": 2
+            },
+            "tag": "boots",
+            "replaceable": True,
+            "destroy_when_lost": False
+        },
+        4: {
+            "name": "Oceľové brnenie (LVL 3)",
+            "description": "Pridáva 5 obrany. Zostáva ti do konca hry, jedine že by si ho nahradil iným brnením.",
+            "ability": {
+                "Obrana": 5
+            },
+            "tag": "armor",
+            "replaceable": True,
+            "destroy_when_lost": False
+        },
+    },
+    4: {
+        1: {
+            "name": "Všehodžús (LVL 4)",
+            "description": "Pridáva 30 života a 10 šťastia. Ak pri súboji zomrieš, tak ti nápoj vyprchá. Môžeš mať viacero nápojov naraz.",
+            "ability": {
+                "Život": 30,
+                "Šťastie": 10
+            },
+            "tag": "drink",
+            "replaceable": False,
+            "destroy_when_lost": True
+        },
+        2: {
+            "name": "Dvojhlavá sekera (LVL 4)",
+            "description": "Pridáva 10 útočnej sily. Zostáva ti do konca hry, jedine že by si ju nahradil inou zbraňou.",
+            "ability": {
+                "Útočná sila": 10
+            },
+            "tag": "weapon",
+            "replaceable": True,
+            "destroy_when_lost": False
+        },
+        3: {
+            "name": "Tréning Spartana (LVL 4)",
+            "description": "Pridáva 3 body skillu, 3 body útočnej sily a 3 body obrany. Schopnosti ti nevyprchajú.",
+            "ability": {
+                "Skill": 3,
+                "Útočná sila": 3,
+                "Obrana": 3
+            },
+            "tag": "training",
+            "replaceable": False,
+            "destroy_when_lost": False
+        },
+        4: {
+            "name": "Rímsky štít (LVL 4)",
+            "description": "Pridáva 16 obrany, ale odoberá 2 body obratnosti. Zostáva ti do konca hry.",
+            "ability": {
+                "Obrana": 16,
+                "Obratnosť": -2
+            },
+            "tag": "armor",
+            "replaceable": False,
+            "destroy_when_lost": False
+        },
+    }
+}
